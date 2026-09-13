7dfc1523370341baf6e91c1585b766823e1d41e7
DamienInge27 <inged4619@student.faytechcc.edu>
2026-09-13 09:41:55 -0400

    Add files via upload

---
+++ p1lab1_ingedamien.py
@@ -0,0 +1,8 @@
+# Damien Inge
+# September 13, 2026
+# P1LAB1
+# Create a Python program that welcomes a user using their first and last name.
+
+first_name = input("Enter your first name: ")
+last_name =input("Enter your last name: ")
+print("Hello," ,first_name, last_name +"!", "Welcome to CTI-110")
